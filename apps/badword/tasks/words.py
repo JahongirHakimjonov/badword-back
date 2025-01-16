2e038fd4cce105ffe1fc1578362db634d30d747e
JahongirHakimjomov <jahongirhakimjonov@gmail.com>
2025-01-16 16:24:14 +0500

    fix: fix

---
+++ apps/badword/tasks/words.py
@@ -0,0 +1,34 @@
+import csv
+import json
+import os
+
+import pandas as pd
+from celery import shared_task
+from django.conf import settings
+
+from apps.badword.models import Word
+
+
+@shared_task
+def update_bad_words():
+    words = Word.objects.filter(is_active=True)
+    data = [{'id': word.id, 'word': word.word} for word in words]
+
+    # Save to CSV
+    csv_file_path = os.path.join(settings.MEDIA_ROOT, 'words', 'bad_words.csv')
+    os.makedirs(os.path.dirname(csv_file_path), exist_ok=True)
+    with open(csv_file_path, 'w', newline='') as csvfile:
+        fieldnames = ['id', 'word']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(data)
+
+    # Save to JSON
+    json_file_path = os.path.join(settings.MEDIA_ROOT, 'words', 'bad_words.json')
+    with open(json_file_path, 'w') as jsonfile:
+        json.dump(data, jsonfile, indent=4)
+
+    # Save to XLSX
+    xlsx_file_path = os.path.join(settings.MEDIA_ROOT, 'words', 'bad_words.xlsx')
+    df = pd.DataFrame(data)
+    df.to_excel(xlsx_file_path, index=False)
--- core/celery.py
+++ core/celery.py
@@ -2,7 +2,7 @@ import os
 
 from celery import Celery
 
-# from celery.schedules import crontab
+from celery.schedules import crontab
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
@@ -10,5 +10,12 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 app = Celery("core")
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
+
+app.conf.beat_schedule = {
+    "update_words": {
+        "task": "apps.badword.tasks.words.update_bad_words",
+        "schedule": crontab(minute=0, hour="*"),
+    },
+}
 
 app.autodiscover_tasks()
