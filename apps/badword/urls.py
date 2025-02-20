5df849ca26552d98929e49d9917a07045ac9d0d2
JahongirHakimjomov <jahongirhakimjonov@gmail.com>
2025-02-20 10:50:32 +0500

    fix: update badword URL path for clarity

--- apps/badword/urls.py
+++ apps/badword/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 
-from apps.badword.views import WordListAPIView, WordListView
+from apps.badword.views import WordListAPIView
 from apps.badword.views.bad_filter import BadWordFilterView
 
 urlpatterns = [
-    # path('', WordListView.as_view(), name='word_list'),
     path("api/v1/badword/", WordListAPIView.as_view(), name="badword"),
-    path("api/v1/badword/check/", BadWordFilterView.as_view(), name="badword"),
+    path("api/v1/badword/check/", BadWordFilterView.as_view(), name="badword-check"),
 ]
-
